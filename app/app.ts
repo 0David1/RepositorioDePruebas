dbd08e779dc90cb3aa0c9a3d53d10fdbd5c17c6b
David <enzobautista1000@gmail.com>
2022-12-12 16:35:09 -0300

    save

--- app/app.ts
+++ app/app.ts
@@ -1,2 +1,27 @@
 let message: string = "Hello world";
 console.log(message);
+
+
+class Persona{
+    private nombre: string;
+    private apellido: string;
+    private añoNac: number;
+  
+    constructor(nombre: string, apellido: string, añoNac: number) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.añoNac = añoNac;
+    }
+  
+    public toString() :string{
+      return this.nombre +" "+ this.apellido;
+    }
+  
+    public edad(añoActual: number){
+      return (añoActual-this.añoNac);
+    }
+  }
+  
+  let persona = new Persona("Enzo", "Bautista", 2001)
+  console.log(persona.toString())
+  console.log(persona.edad(2022))
