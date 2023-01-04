310c99224ea96020b7f67516e3a87e19f4bcef30
David <enzobautista1000@gmail.com>
2023-01-04 00:09:20 -0300

    save

--- PortfolioAngular/src/app/app-routing.module.ts
+++ PortfolioAngular/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { Page404Component } from './page404/page404.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
-  {path:'',redirectTo: '/home', pathMatch: 'full'},
-  {path:'login', component: LoginComponent}
+  {path:'',redirectTo: '/portfolio', pathMatch: 'full'},
+  {path:'login', component: LoginComponent},
+  {path:'portfolio', component: PortfolioComponent},
+  {path:'**', component: Page404Component},
 ];
 
 @NgModule({
--- PortfolioAngular/src/app/app.module.ts
+++ PortfolioAngular/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    PortfolioComponent,
+    Page404Component,
   ],
   imports: [
     BrowserModule,
