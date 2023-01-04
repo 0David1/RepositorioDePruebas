0793a2f18c0e316dc7594e313d8bff4eae3ae166
David <enzobautista1000@gmail.com>
2023-01-03 22:17:23 -0300

    save

--- PortfolioAngular/src/app/app-routing.module.ts
+++ PortfolioAngular/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path:'',redirectTo: '/home', pathMatch: 'full'},
-  {path:'home', component:AppComponent},
+  {path:'login', component: LoginComponent}
 ];
 
 @NgModule({
