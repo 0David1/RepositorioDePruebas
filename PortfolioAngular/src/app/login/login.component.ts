7e7211ab71834093c84f187eb742fb0d2545257b
David <enzobautista1000@gmail.com>
2023-01-05 01:12:39 -0300

    save

--- PortfolioAngular/src/app/login/login.component.ts
+++ PortfolioAngular/src/app/login/login.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  form: FormGroup
 
+  constructor(private FormBuilder: FormBuilder) {
+    this.form = this.FormBuilder.group({
+      email:['', [Validators.required,]],
+      password:['',[Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  ngOnInit() { }
+
+  get Mail(){
+    return this.form.get("email");
+   }
+
+  get MailValid() {
+    return false
+  }
+
+  onEnviar(event: Event){
+    // Detenemos la propagación o ejecución del compotamiento submit de un form
+    event.preventDefault; 
+ 
+    if (this.form.valid){
+      // Llamamos a nuestro servicio para enviar los datos al servidor
+      // También podríamos ejecutar alguna lógica extra
+      alert("Todo salio bien ¡Enviar formuario!")
+    }else{
+      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
+      this.form.markAllAsTouched(); 
+    }
+  }
 }
